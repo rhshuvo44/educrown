06b277c4e71dd8a0e0165a19a5882c365bc982a8
RIPON HOSSAIN SHUVO <rhshuvo44@gmail.com>
2022-08-20 10:57:08 +0600

    banner

---
+++ src/Component/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div id="banner">
+      <div className="banner-info">
+        <h1>Develop a passion for learning new things.</h1>
+        <p>
+          Was certainty remaining engrossed applauded sir how discovery. Settled
+          opinion how enjoyed greater joy adapted too shy. Now properly surprise
+          expenses.
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/Component/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+            <Banner/>
+        </div>
+    );
+};
+
+export default Home;
