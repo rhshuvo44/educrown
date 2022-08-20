ffcc5e2579c935190c9d92bfb1f0295c0df36e03
RIPON HOSSAIN SHUVO <rhshuvo44@gmail.com>
2022-08-20 13:40:16 +0600

    faqs

---
+++ src/Component/Faqs/Faqs.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const Faqs = () => {
+  return (
+    <div className="container py-5">
+      <h2 className="text-center">FAQS</h2>
+      <div className="row py-5">
+        <div className="col-md-4">
+          <h5>1.Two Very Different Australias?</h5>
+          <p>
+            A Google Docs scam that appears to be widespread began landing in
+            users’ inboxes on Wednesday in what seemed to be a sophisticated
+            phishing.
+          </p>
+        </div>
+        <div className="col-md-4">
+          <h5>2.What Does Darwin Mean to You?</h5>
+          <p>
+            A Google Docs scam that appears to be widespread began landing in
+            users’ inboxes on Wednesday in what seemed to be a sophisticated
+            phishing.
+          </p>
+        </div>
+        <div className="col-md-4">
+          <h5>3.Where Totem Poles Are a Living Art ?</h5>
+          <p>
+            A Google Docs scam that appears to be widespread began landing in
+            users’ inboxes on Wednesday in what seemed to be a sophisticated
+            phishing.
+          </p>
+        </div>
+        <div className="col-md-4">
+          <h5>1.Two Very Different Australias?</h5>
+          <p>
+            A Google Docs scam that appears to be widespread began landing in
+            users’ inboxes on Wednesday in what seemed to be a sophisticated
+            phishing.
+          </p>
+        </div>
+        <div className="col-md-4">
+          <h5>2.What Does Darwin Mean to You?</h5>
+          <p>
+            A Google Docs scam that appears to be widespread began landing in
+            users’ inboxes on Wednesday in what seemed to be a sophisticated
+            phishing.
+          </p>
+        </div>
+        <div className="col-md-4">
+          <h5>3.Where Totem Poles Are a Living Art ?</h5>
+          <p>
+            A Google Docs scam that appears to be widespread began landing in
+            users’ inboxes on Wednesday in what seemed to be a sophisticated
+            phishing.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Faqs;
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Courses from '../Courses/Courses';
+import Faqs from '../Faqs/Faqs';
 import Features from '../Features/Features';
+import Navbar from '../Shared/Navbar';
 
 const Home = () => {
     return (
         <div>
-            <h1>Navbar</h1>
+            <Navbar/>
             <Banner/>
             <Features/>
             <Courses/>
+            <Faqs/>
         </div>
     );
 };
